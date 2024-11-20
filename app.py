f8d1e1fd209155efe3c82fd67b1456be26b3a3b1
Ahiwe Onyebuchi Valentine <vahiwe@gmail.com>
2024-11-20 16:43:03 +0100

    Add .streamlit/secrets.toml to .gitignore and update app.py with AWS credentials and bedrock client

--- app.py
+++ app.py
@@ -1,18 +1,46 @@
-import streamlit as st
+import json 
+import os
+import sys
+import boto3
 import random
 import time
+import streamlit as st
 
+AWS_REGION = st.secrets["AWS_REGION"]
+AWS_ACCESS_KEY_ID = st.secrets["AWS_ACCESS_KEY_ID"]
+AWS_SECRET_ACCESS_KEY = st.secrets["AWS_SECRET_ACCESS_KEY"]
 
-# Streamed response emulator
-def response_generator():
-    response = random.choice(
-        [
-            "Hello there! How can I assist you today?",
-            "Hi, human! Is there anything I can help you with?",
-            "Do you need help?",
-        ]
+# Initialize AWS Bedrock client
+session = boto3.Session(
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=AWS_REGION,
+)
+
+bedrock_client = session.client("bedrock-runtime")
+
+# Streamed response generator
+def response_generator(prompt):
+    prompt_data = f"""Command: {prompt}
+    Response:
+    """
+    body = json.dumps(
+        {"inputText": prompt_data, 
+        "textGenerationConfig" : {"topP":0.95, "temperature":0.2}}
     )
-    for word in response.split():
+    modelId = "amazon.titan-tg1-large" 
+    accept = "application/json"
+    contentType = "application/json"
+
+    titan_response = bedrock_client.invoke_model(
+        body=body, 
+        modelId=modelId, 
+        accept=accept, 
+        contentType=contentType
+    )
+    response = json.loads(titan_response.get("body").read())
+    response_text = response.get("results")[0].get("outputText")
+    for word in response_text.split():
         yield word + " "
         time.sleep(0.05)
 
@@ -38,6 +66,6 @@ if prompt := st.chat_input("What is up?"):
 
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
-        response = st.write_stream(response_generator())
+        response = st.write_stream(response_generator(prompt))
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
