09144ec490910423c44e8512a8025613a5873ac4
Ahiwe Onyebuchi Valentine <vahiwe@gmail.com>
2024-11-30 09:42:13 -0100

    Chore: Add Terraform Infra code

---
+++ .infra/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "bedrock-infra-state"
+    key = "terraform.tfstate"
+    use_lockfile = true
+    encrypt = true
+    region = "us-east-1"
+  }
+}
---
+++ .infra/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.48"
+    }
+    opensearch = {
+      source  = "opensearch-project/opensearch"
+      version = "= 2.2.0"
+    }
+    awscc = {
+      source  = "hashicorp/awscc"
+      version = "~> 1.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
